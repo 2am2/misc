7f73ab5b6598abae76561db2ceab381649e02533
2am2 <17.amar1@gmail.com>
2022-06-08 13:40:17 -0400

    updated stuff, vaguely

---
+++ chess.py
@@ -0,0 +1,20 @@
+from asyncio.windows_events import NULL
+from xmlrpc.client import boolean
+
+
+class Piece:
+    def __init__(self) -> None:
+        pass
+
+    def move(self,x,y):
+        
+
+class Pawn(Piece):
+    pass
+
+        
+class Position(self,False,Piece = None):
+    
+    def __init__(self) -> None:
+        ocpd = False
+        pass
---
+++ printtest.py
@@ -0,0 +1,7 @@
+import random
+
+def dice():
+    r = random.randint(1,6)
+    print(r)
+    
+dice()
